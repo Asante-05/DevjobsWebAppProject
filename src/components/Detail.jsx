ab9d29518f6f3f2ecbd6331a1b34aa4c5ba79dbe
Asante <kelvinowusu523@gmail.com>
2023-07-29 03:22:01 -0700

    added responsive webview

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,14 +1,183 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { getItemByID } from "../myAPI/API";
 import { stateContext } from "../Context/StateContext";
 import classNames from "classnames";
+import debounce from "lodash.debounce";
 
 export function Detail({ id }) {
+  const [screenSize, setScreenSize] = useState("small");
+
+  // Optional: Use debounce for window resize event (to avoid excessive re-renders)
+  const handleResize = debounce(() => {
+    const width = window.innerWidth;
+    if (width < 768) {
+      setScreenSize("small");
+    } else {
+      setScreenSize("medium");
+    }
+  }, 300);
+
+  useEffect(() => {
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener on unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, [handleResize]);
+
+  return (
+    <>
+      <div>
+        {screenSize === "small" && <SmallComponentDetail id={id} />}
+        {screenSize === "medium" && <MediumComponentDetail id={id} />}
+      </div>
+    </>
+  );
+}
+
+const MediumComponentDetail = ({ id }) => {
   const { isSelected, setIsSelected } = useContext(stateContext);
 
   const item = getItemByID(id - 1);
-  console.log(isSelected)
+  return (
+    <>
+      <div>
+        <div className="flex justify-center -top-4 items-center   relative">
+          <div className="bg-white w-[70%] rounded-lg flex items-center ">
 
+            <div className="w-[140px] h-[140px]">
+              <img src={item.logo}></img>
+            </div>
+
+            <div className=" w-11/12 rounded-lg flex justify-between ">
+              <div className="gap-5">
+                <div className="text-xl font-bold">{item.company}</div>
+                <div className="text-base">{item.website}</div>
+              </div>
+
+              <div className="mr-8">
+                <button className="h-[48px] w-[147px] rounded text-base font-bold text-violet bg-lightGrey ">
+                  Company Site
+                </button>
+              </div>
+
+
+            </div>
+          </div>
+        </div>
+
+
+
+        
+
+        <div className="flex justify-center items-center relative mt-10">
+          <div
+            className={classNames(
+              " mt-1 w-[70%]  p-10 bg-white rounded-bl-md flex flex-col justify-center gap-5 items-center ",
+              { "bg-veryDarkBlue": isSelected }
+            )}
+          >
+           <div className="flex">
+              <div className="space-y-2">
+                <div className="flex space-x-4 text-grey text-[16] font-medium">
+                  <h1>{item.postedAt}</h1>
+                  <h1>.</h1>
+                  <h1>{item.contract}</h1>
+                </div>
+                <div
+                  className={classNames(
+                    "font-extrabold  text-[20px] text-black",
+                    { "text-white": isSelected }
+                  )}
+                >
+                  <h1>{item.position}</h1>
+                </div>
+                <div className="text-violet font-bold leading-normal ">
+                  <h1>{item.location}</h1>
+                </div>
+              </div>
+
+
+              <div>
+                <button className="h-12 text-white text-base font-extrabold mt-10 bg-violet rounded-md ">
+                  Apply Now
+                </button>
+              </div>
+            </div>
+         
+         
+            
+
+            <div className="mt-5">
+              <div
+                className={classNames("text-black", {
+                  "text-grey": isSelected,
+                })}
+              >
+                <div>{item.description}</div>
+              </div>
+
+              <div className="justify space-y-10">
+                <div className="mt-10 font-bold text-xl">Requirements</div>
+                <div
+                  className={classNames("text-black font-normal text-base", {
+                    "text-grey": isSelected,
+                  })}
+                >
+                  {item.requirements.content}
+                  <h1>requirements</h1>
+                </div>
+                <div
+                  className={classNames("text-black font-normal text-base", {
+                    "text-grey": isSelected,
+                  })}
+                >
+                  {item.requirements.items.map((item) => (
+                    <li>{item}</li>
+                  ))}
+                  <h1>names</h1>
+                </div>
+              </div>
+
+              <div className="justify space-y-10">
+                <div className="mt-10 font-bold text-xl">What You Will Do</div>
+                <div
+                  className={classNames("text-black font-normal text-base", {
+                    "text-grey": isSelected,
+                  })}
+                >
+                  {item.role.content}
+                  <h1>names</h1>
+                </div>
+
+                <div
+                  className={classNames("text-black font-normal text-base", {
+                    "text-grey": isSelected,
+                  })}
+                >
+                  {item.role.items.map((item) => (
+                    <li>{item}</li>
+                  ))}
+                  <h1>names</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center mt-9 bg-white p-5 ">
+        <button className="w-72 h-12 text-white text-base font-extrabold bg-violet rounded-md ">
+          Apply Now
+        </button>
+      </div>
+    </>
+  );
+};
+
+const SmallComponentDetail = ({ id }) => {
+  const { isSelected, setIsSelected } = useContext(stateContext);
+
+  const item = getItemByID(id - 1);
   return (
     <>
       <div>
@@ -20,12 +189,13 @@ export function Detail({ id }) {
           <div
             className={classNames(
               "  -mt-4 w-11/12  p-10 bg-white rounded-md flex flex-col justify-center gap-5 items-center ",
-              { "bg-veryDarkBlue": isSelected })}
+              { "bg-veryDarkBlue": isSelected }
+            )}
           >
             <div className="text-xl font-bold">{item.company}</div>
             <div className="text-base">{item.website}</div>
             <div>
-              <button className="h-[48px] w-[147px] rounded text-base font-bold text-violet bg-lightViolet ">
+              <button className="h-[48px] w-[147px] rounded text-base font-bold text-violet bg-lightGrey ">
                 Company Site
               </button>
             </div>
@@ -33,7 +203,12 @@ export function Detail({ id }) {
         </div>
 
         <div className="flex justify-center items-center relative mt-10">
-          <div className={classNames(" mt-1 w-11/12  p-10 bg-white rounded-bl-md flex flex-col justify-center gap-5 items-center ", {"bg-veryDarkBlue" : isSelected})}>
+          <div
+            className={classNames(
+              " mt-1 w-11/12  p-10 bg-white rounded-bl-md flex flex-col justify-center gap-5 items-center ",
+              { "bg-veryDarkBlue": isSelected }
+            )}
+          >
             <div className=" w-11/12 ">
               <div className="space-y-2">
                 <div className="flex space-x-4 text-grey text-[16] font-medium">
@@ -41,7 +216,12 @@ export function Detail({ id }) {
                   <h1>.</h1>
                   <h1>{item.contract}</h1>
                 </div>
-                <div className={classNames("font-extrabold  text-[20px] text-black", {"text-white" : isSelected})}>
+                <div
+                  className={classNames(
+                    "font-extrabold  text-[20px] text-black",
+                    { "text-white": isSelected }
+                  )}
+                >
                   <h1>{item.position}</h1>
                 </div>
                 <div className="text-violet font-bold leading-normal ">
@@ -55,17 +235,29 @@ export function Detail({ id }) {
             </div>
 
             <div className="mt-5">
-              <div className={classNames("text-black", {"text-grey" : isSelected})}>
+              <div
+                className={classNames("text-black", {
+                  "text-grey": isSelected,
+                })}
+              >
                 <div>{item.description}</div>
               </div>
 
               <div className="justify space-y-10">
                 <div className="mt-10 font-bold text-xl">Requirements</div>
-                <div className={classNames("text-black font-normal text-base", {"text-grey" : isSelected})}>
+                <div
+                  className={classNames("text-black font-normal text-base", {
+                    "text-grey": isSelected,
+                  })}
+                >
                   {item.requirements.content}
                   <h1>requirements</h1>
                 </div>
-                <div className={classNames("text-black font-normal text-base", {"text-grey" : isSelected})}>
+                <div
+                  className={classNames("text-black font-normal text-base", {
+                    "text-grey": isSelected,
+                  })}
+                >
                   {item.requirements.items.map((item) => (
                     <li>{item}</li>
                   ))}
@@ -75,12 +267,20 @@ export function Detail({ id }) {
 
               <div className="justify space-y-10">
                 <div className="mt-10 font-bold text-xl">What You Will Do</div>
-                <div className={classNames("text-black font-normal text-base", {"text-grey" : isSelected})}>
+                <div
+                  className={classNames("text-black font-normal text-base", {
+                    "text-grey": isSelected,
+                  })}
+                >
                   {item.role.content}
                   <h1>names</h1>
                 </div>
 
-                <div className={classNames("text-black font-normal text-base", {"text-grey" : isSelected})}>
+                <div
+                  className={classNames("text-black font-normal text-base", {
+                    "text-grey": isSelected,
+                  })}
+                >
                   {item.role.items.map((item) => (
                     <li>{item}</li>
                   ))}
@@ -90,14 +290,14 @@ export function Detail({ id }) {
             </div>
           </div>
         </div>
-
       </div>
-        <div className="flex justify-center mt-9 bg-white p-5 ">
-          <button className="w-72 h-12 text-white text-base font-extrabold bg-violet rounded-md ">
-            Apply Now
-          </button>
-        </div>
+      <div className="flex justify-center mt-9 bg-white p-5 ">
+        <button className="w-72 h-12 text-white text-base font-extrabold bg-violet rounded-md ">
+          Apply Now
+        </button>
+      </div>
     </>
   );
-}
+};
+
 export default Detail;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,8 @@
 import Filter from "./Filter";
 import iconFilter from "/assets/mobile/icon-filter.svg";
 import iconSearch from "/assets/desktop/icon-search.svg";
+import iconLocation from "/assets/desktop/icon-location.svg";
+
 import React, { useContext, useState, useEffect } from "react";
 import classNames from "classnames";
 import { stateContext } from "../Context/StateContext";
@@ -15,11 +17,9 @@ export function SearchBar() {
     const width = window.innerWidth;
     if (width < 768) {
       setScreenSize("small");
-    } else if (width >= 768 && width < 1024) {
-      setScreenSize("medium");
     } else {
-      setScreenSize("large");
-    }
+      setScreenSize("medium");
+    } 
   }, 300);
 
   useEffect(() => {
@@ -35,16 +35,11 @@ export function SearchBar() {
       <div>
         {screenSize === "small" && <SmallComponent />}
         {screenSize === "medium" && <MediumComponent />}
-        {screenSize === "large" && <LargeComponent />}
+        
       </div>
     </>
   );
 }
-
-
-
-
-
 
 const SmallComponent = () => {
   const [filter, setFilter] = useState(false);
@@ -113,7 +108,62 @@ const SmallComponent = () => {
     </>
   );
 };
-const MediumComponent = () => <p>This is a medium component.</p>;
+const MediumComponent = () => {
+  const [filter, setFilter] = useState(false);
+  const { isSelected, setIsSelected, updated, setUpdated } =
+    useContext(stateContext);
+  const [message, setMessage] = useState();
+  const handleClick = () => {
+    setUpdated(message);
+  };
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  };
+  return (
+    <>
+      <div
+        className={classNames(
+          "mx-auto w-11/12 p-3 -top-8  relative left-0 right-0  rounded-lg bg-white ",
+          {
+            "bg-red transition-all duration-500": isSelected,
+          }
+        )}
+      >
+        <div
+          className={classNames("flex justify-between items-center pl-3", {
+            "bg-veryDarkBlue": isSelected,
+          })}
+        >
+          <div className={classNames("w-1/3 flex gap-2")}>
+            <img src={iconSearch} alt="x" className=""></img>
+            <input type="text" placeholder="Filter by title" className="w-[85%]"></input>
+          </div>
+          <div className="h-16 w-[1px] bg-grey"></div>
+          <div className={classNames("w-1/3 flex gap-3  ml-2")}>
+            <img src={iconLocation} alt="x" className=""></img>
+
+            <input type="text" placeholder="Filter by location" className="w-[85%]"></input>
+          </div>
+          <div className="h-16 w-[1px] bg-grey"></div>
+
+          <div className={classNames("w-1/3 flex justify-center items-center gap-7 ml-2")}>
+            <div>
+              <input
+                type="checkbox"
+                className="max-w-[60px] max-h-[60px] bg-grey"
+              ></input>
+              <label className="text-[15px] font-semibold">Full Time</label>
+            </div>
+            <div className="max-w-[80px] max-h-[48px] ">
+              <button className="font-extrabold">Search</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
+};
 const LargeComponent = () => <p>This is a large component.</p>;
 
 export default SearchBar;
